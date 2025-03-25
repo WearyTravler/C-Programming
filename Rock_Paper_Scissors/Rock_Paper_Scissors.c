f71daeedbd1ae72dca7711618f94b228ac624d8b
WearyTravler <limbo_shell@protonmail.com>
2025-03-25 00:14:22 -0500

    added initial files.
    Signed-off-by: WearyTravler <limbo_shell@protonmail.com>

---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int game(char player, char machine) 
+{
+    // Machine and player both choosing the same choice
+    if (player == machine)
+        return -1;
+
+    // player
+}
